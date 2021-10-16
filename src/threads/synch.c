7567eb5e94f6cf0cdff492e96446cebe107dd120
Charles Loveman <cel20@ic.ac.uk>
2021-10-16 15:38:30 +0100

    fix: increase semaphore value before unblocking

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -112,10 +112,10 @@ sema_up_max (struct semaphore *sema, int64_t max) {
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
+  sema->value = MIN(sema->value + 1, max);
   if (!list_empty (&sema->waiters)) 
     thread_unblock (list_entry (list_pop_max (&sema->waiters, compare_priority, NULL),
                                 struct thread, elem));
-  sema->value = MIN(sema->value + 1, max);
   intr_set_level (old_level);
 }
 
