5c23fdc3cf07d8027d0f2c5b5f06fafd55fb33c4
la320 <la320@ic.ac.uk>
2021-11-01 15:30:38 +0000

    feature: added templates for future syscall functions in syscall.c\nfeature: modified page fault handler for dereferencing user pointers

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -144,15 +144,17 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+  f->eip = f->eax; 
+  f->eax = 0xffffffff;
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
+  /* printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
-  kill (f);
+  kill (f); */
 }
 
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,73 @@
 
 static void syscall_handler (struct intr_frame *);
 
+static void
+halt (void)
+{
+
+}
+
+static void
+exit (int status)
+{
+
+}
+
+/* Tasks 2 and later. */
+static void 
+halt (void)
+{
+}
+static void 
+exit (int status)
+{
+}
+static pid_t 
+exec (const char *file)
+{
+}
+static int 
+wait (pid_t)
+{
+}
+static bool 
+create (const char *file, unsigned initial_size)
+{
+}
+static bool 
+remove (const char *file)
+{
+}
+static int 
+open (const char *file)
+{
+}
+static int 
+filesize (int fd)
+{
+}
+static int 
+read (int fd, void *buffer, unsigned length)
+{
+}
+static int 
+write (int fd, const void *buffer, unsigned length)
+{
+}
+static void 
+seek (int fd, unsigned position)
+{
+}
+static unsigned 
+tell (int fd)
+{
+}
+static void 
+close (int fd)
+{
+}
+
+#endif /* lib/user/syscall.h */
 void
 syscall_init (void) 
 {
@@ -13,8 +80,43 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
+  uint32_t intr =  *((uint32_t *) f->esp);
+  switch (intr):
+	case SYS_HALT:
+	  halt();
+
+    
   thread_exit ();
 }
+
+/* Reads a byte at user virtual address UADDR.
+UADDR must be below PHYS_BASE.
+Returns the byte value if successful, -1 if a segfault
+occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  ASSERT (is_user_vaddr(uaddr)); //checks uaddr is below PHYS_BASE
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+  : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{ 
+  ASSERT (is_user_vaddr(udst)); //checks udst is below PHYS_BASE
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+  : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
+
+
