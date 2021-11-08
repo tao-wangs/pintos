904627101e3637539794b4277f20fba3c5edc783
as3820 <as3820@imperial.ac.uk>
2021-11-08 07:34:50 -0800

    feat: Attempting to implement the argument passing to the stack, and moving put_user() and get_user()

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -41,10 +41,39 @@ process_execute (const char *file_name)
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (program_name, PRI_DEFAULT, start_process, fn_copy);
+//  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
+  
+}
+
+/* Reads a byte at user virtual address UADDR.
+UADDR must be below PHYS_BASE.
+Returns the byte value if successful, -1 if a segfault
+occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  ASSERT (is_user_vaddr(uaddr)); //checks uaddr is below PHYS_BASE
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+  : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{ 
+  ASSERT (is_user_vaddr(udst)); //checks udst is below PHYS_BASE
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+  : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
 }
 
 /* A thread function that loads a user process and starts it
@@ -68,6 +97,14 @@ start_process (void *file_name_)
   if (!success) 
     thread_exit ();
 
+  int argc = (arguments.length / sizeof(char *));
+  for (int i = argc; i!= 0; i--) {
+  	char * argument = arguments[i];
+  	char * argument_with_null = argument + "\0";
+  	uint8_t address_with_null_pointer = uint8_t *(argument_with_null);
+  	put_user(
+  }
+  
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
      threads/intr-stubs.S).  Because intr_exit takes all of its
