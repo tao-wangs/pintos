d12c7f0997816c057b5d7b53fd3cf9b1e0ccb5ab
Tao Wang <tw520@imperial.ac.uk>
2021-11-11 15:30:30 +0000

    fix: compilation warnings by including type declarations for static methods in syscall.c. Ignore arguments never used warnings because those functions are being implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,6 +10,24 @@ typedef int pid_t;
 
 static void syscall_handler (struct intr_frame *);
 
+static void halt(void);
+static void exit(int status);
+static pid_t exec(const char *file);
+static int wait(pid_t pid);
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned length);
+static int write (int fd, const void *buffer, unsigned length);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
+
+static int get_user (const uint8_t *uddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+static int get_int (const uint8_t *uddr);
+
 /* Reads a byte at user virtual address UADDR.
 UADDR must be below PHYS_BASE.
 Returns the byte value if successful, -1 if a segfault
@@ -75,14 +93,17 @@ exec (const char *file)
 {
   return process_execute(file);
 }
+
 static int 
 wait (pid_t pid)
 {
 }
+
 static bool 
 create (const char *file, unsigned initial_size)
 {
 }
+
 static bool 
 remove (const char *file)
 {
