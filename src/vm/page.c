d5bde38c2b26dd475a0e9f40f8abd574a6dc666d
Charles Loveman <cel20@ic.ac.uk>
2021-11-29 16:07:50 +0000

    feat: Add initial supplemental page table.

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,1 +1,85 @@
-#include "page.h"
+#include "vm/page.h"
+#include <hash.h>
+#include "threads/synch.h"
+
+struct hash page_table;
+
+struct lock page_lock;
+
+static unsigned
+page_hash (const struct hash_elem *e, void *aux UNUSED)
+{
+  struct page* elem = hash_entry (e, struct page, elem);
+  return hash_int ((int) elem->addr);
+}
+
+static bool
+page_less (const struct hash_elem *a,
+           const struct hash_elem *b,
+           void *aux UNUSED)
+{
+  return hash_entry (a, struct page, elem)->addr
+       < hash_entry (b, struct page, elem)->addr;
+}
+
+void 
+pagetable_init ()
+{
+  hash_init (&page_table, page_hash, page_less, NULL);
+  lock_init (&page_lock);
+}
+
+struct page *
+locate_page (void *addr)
+{
+  void *page = pg_round_down (addr); 
+  struct page temp; 
+  temp.page = page;
+  acquire_lock (&page_lock);
+  struct hash_elem *e = hash_find (&page_table, &temp.elem);
+  release_lock (&page_lock);
+  if (!e)
+  {
+    return NULL;  
+  }
+  return hash_entry (e, struct page, elem);
+}
+
+void
+add_page (void *addr, void *data, enum page_status status)
+{
+  void *pg_addr = pg_round_down (addr);
+  struct page *page = malloc (sizeof (struct page));
+  if (!page)
+  {
+    PANIC ("failed to allocate supplemental page table entry");
+  }
+  page->addr = pg_addr;
+  page->data = data;
+  page->status = status;
+  lock_acquire (&page_table);
+  hash_insert (&page_table, &page->elem);
+  lock_release (&page_table);
+}
+
+void
+remove_page (void *addr)
+{
+  struct page *page = locate_page (page); 
+  if (!page)
+    return;
+  lock_acquire (&page_lock);
+  hash_delete (&page_table, &page->elem);
+  lock_release (&page_lock);
+  free (page);
+}
+static void
+page_remove (struct hash_elem *e, void *aux UNUSED)
+{
+  free (hash_entry (e, struct page, elem));
+}
+
+void pagetable_destroy ()
+{
+  hash_destroy (&page_table, page_remove);
+}
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,6 +1,26 @@
 #ifndef VM_PAGE_H
 #define VM_PAGE_H
 
-//empty for now
+#include <hash.h>
 
+enum page_status
+{
+  FRAME, SWAP, FILESYS
+};
+
+struct page
+{
+  struct hash_elem elem;
+  void *addr;
+  enum page_status status;
+  void *data;
+};
+
+void pagetable_init ();
+
+struct page *locate_page (void *addr);
+
+void add_page (void *addr, void *data, enum page_status status);
+
+void remove_page (void *addr);
 #endif /* vm/page.h */
