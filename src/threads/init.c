35d180ceb0b439f1038c9698bcd04ac9ba25dc8b
Charles Loveman <cel20@ic.ac.uk>
2021-12-01 13:33:46 +0000

    fix: Initialise page table

--- src/threads/init.c
+++ src/threads/init.c
@@ -38,6 +38,7 @@
 #include "filesys/fsutil.h"
 #endif
 #include "vm/frame.h"
+#include "vm/page.h"
 
 /* Page directory with kernel mappings only. */
 uint32_t *init_page_dir;
@@ -98,7 +99,8 @@ main (void)
   /* Initialize memory system. */
   palloc_init (user_page_limit);
   malloc_init ();
-  frametable_init();
+  frametable_init ();
+  pagetable_init ();
   paging_init ();
 
   /* Segmentation. */
